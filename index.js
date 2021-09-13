0a9fc90719211ce0420b6980a7576b57958fbec5
monteirovictor <victormonteiromaia95@gmail.com>
2021-09-13 16:35:04 -0300

    alteração index

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const userRoute = require('./routes/userRoute')
+
 const app = express()
 const port = 3000
+
+userRoute(app)
 
 app.get('/',(req,res)=> res.send('Olá mundo express'))
 
