8b33702d06634afb9562cac4d37d3014297276e5
monteirovictor <victormonteiromaia95@gmail.com>
2021-09-13 16:34:41 -0300

    criado get

---
+++ routes/userRoute.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+const { join } = require('path')
+
+const filePath = join(__dirname,'users.json')
+
+const getUsers = ()=>{
+    const data = fs.existsSync(filePath)
+    ?fs.readFileSync(filePath)
+    :[]
+    try {
+        return JSON.parse(data)
+    } catch (error) {
+        return []
+    }
+}
+
+const saveUser = (users) => fs.writeFileSync(filePath, JSON.stringify(users,null,'\t'))
+
+const userRoute = (app)=>{
+    
+    app.route('/users/:id?')
+        .get((req,res)=>{
+            const users = getUsers()
+
+            res.send({users})
+        })
+
+        .post((req,res)=>{
+            const users = getUsers()
+            
+            users.push(res.body)
+
+            saveUser(users)
+            res.send({users})
+        })
+}
+
+module.exports=userRoute
